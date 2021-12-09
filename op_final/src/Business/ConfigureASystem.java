9f307e0d0d150510645793c95577700c1f3abcfa
KunwarChopra <you@example.com>
2021-12-08 19:12:29 -0500

    added new packages

--- op_final/src/Business/ConfigureASystem.java
+++ op_final/src/Business/ConfigureASystem.java
@@ -4,10 +4,33 @@
  */
 package Business;
 
+import Business.Person.Person;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
 /**
  *
  * @author kunwa
  */
 public class ConfigureASystem {
-    
+    public static Ecosystem configure(){
+        
+        Ecosystem system = Ecosystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Person person = system.getPersonDirectory().createPerson("RRH");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", person, new SystemAdminRole(),"");
+        
+        return system;
+    }
+ 
 }
+    
+
--- op_final/src/Business/Ecosystem.java
+++ op_final/src/Business/Ecosystem.java
@@ -4,10 +4,56 @@
  */
 package Business;
 
+import Business.Network.Network;
+import Business.Organization.Organization;
+import Business.Role.Role;
+import Business.Role.SystemAdminRole;
+import java.util.ArrayList;
+
 /**
  *
  * @author kunwa
  */
-public class Ecosystem {
-    
+public class Ecosystem extends Organization {
+
+    private static Ecosystem business;
+    private ArrayList<Network> networkList;
+
+    public static Ecosystem getInstance() {
+        if (business == null) {
+            business = new Ecosystem();
+        }
+        return business;
+    }
+
+    public Network createAndAddNetwork() {
+        Network network = new Network();
+        networkList.add(network);
+        return network;
+    }
+
+    public void deleteNetwork(Network network) {
+        networkList.remove(network);
+    }
+
+    // @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList = new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+
+    private Ecosystem() {
+        super(null);
+        networkList = new ArrayList<Network>();
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+
 }
