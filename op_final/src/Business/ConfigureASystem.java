504078e7bdc404f96ff56da4b6b649fe2d47e23c
KunwarChopra <you@example.com>
2021-12-09 01:18:38 -0500

    added person entity...

--- op_final/src/Business/ConfigureASystem.java
+++ op_final/src/Business/ConfigureASystem.java
@@ -13,24 +13,21 @@ import Business.UserAccount.UserAccount;
  * @author kunwa
  */
 public class ConfigureASystem {
-    public static Ecosystem configure(){
-        
+
+    public static Ecosystem configure() {
+
         Ecosystem system = Ecosystem.getInstance();
-        
+
         //Create a network
         //create an enterprise
         //initialize some organizations
         //have some employees 
         //create user account
-        
-        
         Person person = system.getPersonDirectory().createPerson("RRH");
-        
-        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", person, new SystemAdminRole(),"");
-        
+
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", person, new SystemAdminRole(), "");
+
         return system;
     }
- 
-}
-    
 
+}
