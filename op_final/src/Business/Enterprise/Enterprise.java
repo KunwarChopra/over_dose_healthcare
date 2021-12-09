297248bc3b23ba0950c6635848d98f944bf9236e
SourabhTripathi21 <Tripathi.S@northeastern.edu>
2021-12-08 23:40:11 -0500

    Resolved issues related to enterprise and organization

--- op_final/src/Business/Enterprise/Enterprise.java
+++ op_final/src/Business/Enterprise/Enterprise.java
@@ -4,10 +4,55 @@
  */
 package Business.Enterprise;
 
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+
 /**
  *
  * @author kunwa
  */
-public class Enterprise {
-    
+public abstract class Enterprise extends Organization {
+
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+
+    public enum EnterpriseType {
+        Hospital("Hospital"),
+        RehabCenters("RehabCenters"),
+        Pharmacies("Pharmacies"),
+        Suppliers("Suppliers");
+
+        private String value;
+
+        private EnterpriseType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public Enterprise(String name, EnterpriseType type) {
+        super(name);
+        this.enterpriseType = type;
+        organizationDirectory = new OrganizationDirectory();
+    }
 }
