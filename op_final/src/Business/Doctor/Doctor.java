b486dca5255fea7122f4f79653424501338bafa9
KunwarChopra <you@example.com>
2021-12-09 23:10:58 -0500

    added docotor class.. and made few modification...

---
+++ op_final/src/Business/Doctor/Doctor.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Doctor;
+
+/**
+ *
+ * @author kunwa
+ */
+public class Doctor {
+    
+    String fName;
+    String lName;
+    String licensestatus;
+
+    public String getfName() {
+        return fName;
+    }
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+    public String getlName() {
+        return lName;
+    }
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+    public String getLicensestatus() {
+        return licensestatus;
+    }
+    public void setLicensestatus(String licensestatus) {
+        this.licensestatus = licensestatus;
+    }
+}
+
