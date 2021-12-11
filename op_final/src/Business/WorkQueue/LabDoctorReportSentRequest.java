a889a2822922b58c7567bca7c51092762d330960
Anusha Das <das.anus@northeastern.edu>
2021-12-10 22:08:52 -0500

    modifications..

--- op_final/src/Business/WorkQueue/LabDoctorReportSentRequest.java
+++ op_final/src/Business/WorkQueue/LabDoctorReportSentRequest.java
@@ -8,6 +8,69 @@ package Business.WorkQueue;
  *
  * @author kunwa
  */
-public class LabDoctorReportSentRequest {
-    
+public class LabDoctorReportSentRequest extends WorkRequest {
+    private String patientName;
+    private String patientGender;
+    private int patientAge;
+    private String healthRisk;
+    private String medicalDoseDetail;
+    private String medicalHistory;
+    private String labAssessmentResult;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientGender() {
+        return patientGender;
+    }
+
+    public void setPatientGender(String patientGender) {
+        this.patientGender = patientGender;
+    }
+
+    public int getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(int patientAge) {
+        this.patientAge = patientAge;
+    }
+
+    public String getHealthRisk() {
+        return healthRisk;
+    }
+
+    public void setHealthRisk(String healthRisk) {
+        this.healthRisk = healthRisk;
+    }
+
+    public String getMedicalDoseDetail() {
+        return medicalDoseDetail;
+    }
+
+    public void setMedicalDoseDetail(String medicalDoseDetail) {
+        this.medicalDoseDetail = medicalDoseDetail;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+
+    public String getLabAssessmentResult() {
+        return labAssessmentResult;
+    }
+
+    public void setLabAssessmentResult(String labAssessmentResult) {
+        this.labAssessmentResult = labAssessmentResult;
+    }
 }
+
